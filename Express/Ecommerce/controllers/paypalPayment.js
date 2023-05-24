9dce1d1c19485de775e829239352ba119f4a1485
Ankush Thakur <atpara99@gmail.com>
2023-05-24 17:07:39 +0530

    E-Commerce Application

---
+++ Express/Ecommerce/controllers/paypalPayment.js
@@ -0,0 +1,41 @@
+const braintree = require('braintree');
+
+const gateway = new braintree.BraintreeGateway({
+  environment: braintree.Environment.Sandbox,
+  merchantId: process.env.MERCHANTID,
+  publicKey: process.env.PUBLICKEY,
+  privateKey: process.env.PRIVATEKEY,
+});
+
+exports.getToken = (req, res) => {
+  gateway.clientToken.generate({}, function (error, response) {
+    if (error) {
+      res.status(500).send(error);
+    } else {
+      res.send(response);
+    }
+  });
+};
+
+exports.processPayment = (req, res) => {
+  let nonceFromTheClient = req.body.paymentMethodNonce;
+  let amountFromTheClient = req.body.amount;
+
+  gateway.transaction.sale(
+    {
+      amount: amountFromTheClient,
+      paymentMethodNonce: nonceFromTheClient,
+
+      options: {
+        submitForSettlement: true,
+      },
+    },
+    function (error, result) {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+};
