2c09f79a564ed1fffc5c4ac84fc48866799afb16
Ankush Thakur <atpara99@gmail.com>
2023-06-06 23:26:29 +0530

    Backend Portion completed

--- Express/20PostsServer/index.js
+++ Express/20PostsServer/index.js
@@ -1,29 +1,30 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const postRouter = require("./Routes/PostRoute");
-const bodyParser = require("body-parser");
 dotenv.config();
+const dotenv = require('dotenv');
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const postRouter = require('./Routes/PostRoute');
+const bodyParser = require('body-parser');
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("connected");
+    console.log('connected');
   } catch (error) {
     console.log(`error in mongodb connection`);
   }
 };
 
-app.use("/", bodyParser.json());
+app.use('/', bodyParser.json());
 
-app.use("/api/v1/posts", postRouter);
-app.use("/", (req, res) => {
-  res.send("hello world");
+app.use('/api/v1/posts', postRouter);
+app.use('/', (req, res) => {
+  res.send('hello world');
 });
 
 app.listen(3000, () => {
-  console.log("hello");
+  console.log('hello');
 });
 
 connectDB();
