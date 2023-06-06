295aa422d60cb8dc0b0edfcb5d4ce28dd616f564
Ankush Thakur <atpara99@gmail.com>
2023-06-06 22:23:10 +0530

    Task Manager App Completed

---
+++ React Projects/task_manager_app/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Login from './Login';
+import Dashboard from './Dashboard';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
